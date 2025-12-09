63e20d1cb170454dd3e54eca5145f6dd6f57a118
um-shuhaib <shuhaib5560@gmail.com>
2025-12-09 13:18:19 +0530

    input

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 function Home() {
+
+    const [numbers,setNumbers] = useState({num1:"",num2:""})
+
+    function findsum(){
+        console.log(Number(numbers.num1)+Number(numbers.num2));
+        
+        
+    }
+    console.log(numbers);
+    
+
   return (
     <>
-    <h1 className='text-center'>Home</h1>
-    <Container className='bg-danger'>
-      <Row>
-        <Col className='bg-primary'>1 of 3</Col>
-        <Col className='bg-secondary'>2 of 3</Col>
-        <Col className='bg-warning'>3 of 3</Col>
-      </Row>
-    </Container>
+    <input type="text" placeholder='num1' onChange={(e)=>(setNumbers({...numbers,num1:e.target.value}))} />
+    <input type="text" placeholder='num2' onChange={(e)=>(setNumbers({...numbers,num2:e.target.value}))} />
+    <button className='btn btn-success' onClick={findsum}>Sum</button>
     </>
 
   )
